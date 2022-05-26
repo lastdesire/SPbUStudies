5258976b37382b2039d67eb49ebce90150bd3103
lastdesire <deadinsidezz123450@mail.ru>
2022-05-26 21:48:41 +0300

    Uploaded final version on bashAnalog

--- SecondSemester/Programming/Block_4/Bash/Bash/BashOutput/Logger.cs
+++ SecondSemester/Programming/Block_4/Bash/Bash/BashOutput/Logger.cs
@@ -4,8 +4,11 @@ using Bash.Bash;
 
 namespace Bash.BashOutput
 {
-    public class Logger
+    // Логгер нужен для того, чтобы считывать команды и выводить результат их работы в консоль.
+    public class Logger 
     {
+        // Переменная, содержащая вывод в консоль последней команды. Нужна, в первую очередь, для Unit-тестов
+        // (сравнивать то, что было выведено в консоль, с тем, что должно получаться на самом деле).
         public string lastConsoleWrite = "";
 
         public string ReadCommand()
@@ -18,7 +21,9 @@ namespace Bash.BashOutput
 
             var answer = Console.ReadLine();
 
-            if (answer.Equals("script"))
+            // Если введено слово скрипт, то создаем объект от класса ScriptExecuter, метод Run которого просто возвращает строку,
+            // состоящую из команд. Как будто пользователь сам ввел все то, что в файле. Однако на деле он просто указывает путь к файлу.
+            if (answer.Equals("script")) 
             {
                 var scriptExecuter = new ScriptExecuter();
 
@@ -28,12 +33,15 @@ namespace Bash.BashOutput
                 var script = scriptExecuter.Run(path);
                 return script;
             }
+
+            // Иначе просто работаем со строкой, введенной пользователем.
             else
             {
                 return answer;
             }
         }
 
+        // Ниже представлены методы, печатающие результат работы команды, введенной пользователем.
         public void PrintCommandResult(string[] result)
         {
             Console.ForegroundColor = ConsoleColor.Green;
@@ -41,9 +49,12 @@ namespace Bash.BashOutput
             var counter = 1;
             foreach(var item in result)
             {
-                if (result.Length == counter)
+                // Удаление последнего пробела.
+                // Просто особенность сложностей работы с парсом строк.
+                // Воспринимать как обычный вывод всех элементов массива result.
+                if (result.Length == counter) 
                 {
-                    lastConsoleWrite += item.Substring(0, item.Length - 1);
+                    lastConsoleWrite += item.Substring(0, item.Length -1);
                     Console.Write(item.Substring(0, item.Length - 1));
                 }
                 else
